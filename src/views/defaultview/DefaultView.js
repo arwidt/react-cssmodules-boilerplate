b1a15d5542fa1debb09fa26855a82683719a3af4
Arwid Thornström <arwidt@gmail.com>
2018-03-09 22:13:45 +0100

    Why:
    * first commit
    
    This change addresses the need by:
    
    *

---
+++ src/views/defaultview/DefaultView.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import styles from './DefaultView.scss';
+
+class DefaultView extends Component {
+
+    render() {
+        return (
+            <div className={styles.hello}>HELLO</div>
+        );
+    }
+}
+
+export default DefaultView;
